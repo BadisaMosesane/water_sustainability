d5efab950ff41ee8150d923bc2282d7fe29661ca
BadisaMosesane <badisamosne@gmail.com>
2020-11-14 14:40:54 +0200

    removed satellite map and faulty graphs

--- waterLytics_dashboard/app.py
+++ waterLytics_dashboard/app.py
@@ -61,11 +61,11 @@ layout = dict(
     plot_bgcolor="#F9F9F9",
     paper_bgcolor="#F9F9F9",
     legend=dict(font=dict(size=10), orientation="h"),
-    title="Satellite Overview",
+    title="Map Locations",
     mapbox=dict(
         accesstoken=mapbox_access_token,
         style="light",
-        center=dict(lon=-78.05, lat=42.54),
+        center=dict(lon=-22.35, lat=-34.53),
         zoom=7,
     ),
 )
@@ -81,7 +81,7 @@ app.layout = html.Div(
                 html.Div(
                     [
                         html.Img(
-                            src=app.get_asset_url("dash-logo.png"),
+                            src=app.get_asset_url("waterlytics-logo.png"),
                             id="plotly-image",
                             style={
                                 "height": "60px",
@@ -141,7 +141,7 @@ app.layout = html.Div(
                 html.Div(
                     [
                         html.P(
-                            "Filter by water use date (select date range):",
+                            "Filter by date (select date range):",
                             className="control_label",
                         ),
                         dcc.RangeSlider(
@@ -170,12 +170,12 @@ app.layout = html.Div(
                             value=list(WELL_STATUSES.keys()),
                             className="dcc_control",
                         ),
-                        dcc.Checklist(
-                            id="lock_selector",
-                            options=[{"label": "Lock camera", "value": "locked"}],
-                            className="dcc_control",
-                            value=[],
-                        ),
+                        # dcc.Checklist(
+                        #     id="lock_selector",
+                        #     options=[{"label": "Lock camera", "value": "locked"}],
+                        #     className="dcc_control",
+                        #     value=[],
+                        # ),
                         html.P("Water-Use Restrictions: ", className="control_label"),
                         dcc.RadioItems(
                             id="well_type_selector",
@@ -239,27 +239,27 @@ app.layout = html.Div(
             ],
             className="row flex-display",
         ),
+        # html.Div(
+        #     [
+        #         # html.Div(
+        #         #     [dcc.Graph(id="main_graph")],
+        #         #     className="pretty_container seven columns",
+        #         # ),
+        #         # html.Div(
+        #         #     [dcc.Graph(id="individual_graph")],
+        #         #     className="pretty_container five columns",
+        #         # ),
+        #     ],
+        #     className="row flex-display",
+        # ),
         html.Div(
             [
                 html.Div(
-                    [dcc.Graph(id="main_graph")],
+                    [dcc.Graph(id="bar_graph")], # changed pie to bar
                     className="pretty_container seven columns",
                 ),
                 html.Div(
-                    [dcc.Graph(id="individual_graph")],
-                    className="pretty_container five columns",
-                ),
-            ],
-            className="row flex-display",
-        ),
-        html.Div(
-            [
-                html.Div(
-                    [dcc.Graph(id="pie_graph")],
-                    className="pretty_container seven columns",
-                ),
-                html.Div(
-                    [dcc.Graph(id="aggregate_graph")],
+                    [dcc.Graph(id="aggregate_graph")], # aggregate to bar
                     className="pretty_container five columns",
                 ),
             ],
@@ -571,97 +571,97 @@ def make_aggregate_figure(well_statuses, well_types, year_slider, main_graph_hov
     selected = dff[dff["Well_Type"] == well_type]["API_WellNo"].values
     index, gas, oil, water = produce_aggregate(selected, year_slider)
 
-    data = [
-        dict(
-            type="scatter",
-            mode="lines",
-            name="Fluoride Dissolved (mg/l)",
-            x=index,
-            y=gas,
-            line=dict(shape="spline", smoothing="2", color="#F9ADA0"),
-        ),
-        dict(
-            type="scatter",
-            mode="lines",
-            name="Calcium Dissolved (mg/l)",
-            x=index,
-            y=oil,
-            line=dict(shape="spline", smoothing="2", color="#849E68"),
-        ),
-        dict(
-            type="scatter",
-            mode="lines",
-            name="Static Water Level (mbgs)",
-            x=index,
-            y=water,
-            line=dict(shape="spline", smoothing="2", color="#59C3C3"),
-        ),
-    ]
-    layout_aggregate["title"] = "Aggregate: " + WELL_TYPES[well_type]
+    # data = [
+    #     dict(
+    #         type="scatter",
+    #         mode="lines",
+    #         name="Fluoride Dissolved (mg/l)",
+    #         x=index,
+    #         y=gas,
+    #         line=dict(shape="spline", smoothing="2", color="#F9ADA0"),
+    #     ),
+    #     dict(
+    #         type="scatter",
+    #         mode="lines",
+    #         name="Calcium Dissolved (mg/l)",
+    #         x=index,
+    #         y=oil,
+    #         line=dict(shape="spline", smoothing="2", color="#849E68"),
+    #     ),
+    #     dict(
+    #         type="scatter",
+    #         mode="lines",
+    #         name="Static Water Level (mbgs)",
+    #         x=index,
+    #         y=water,
+    #         line=dict(shape="spline", smoothing="2", color="#59C3C3"),
+    #     ),
+    # ]
+    # layout_aggregate["title"] = "Aggregate: " + WELL_TYPES[well_type]
 
-    figure = dict(data=data, layout=layout_aggregate)
-    return figure
+    # figure = dict(data=data, layout=layout_aggregate)
+    # return figure
 
 
 # Selectors, main graph -> pie graph
-@app.callback(
-    Output("pie_graph", "figure"),
-    [
-        Input("well_statuses", "value"),
-        Input("well_types", "value"),
-        Input("year_slider", "value"),
-    ],
-)
-def make_pie_figure(well_statuses, well_types, year_slider):
+# @app.callback(
+#     Output("pie_graph", "figure"),
+#     [
+#         Input("well_statuses", "value"),
+#         Input("well_types", "value"),
+#         Input("year_slider", "value"),
+#     ],
+# )
+# def make_pie_figure(well_statuses, well_types, year_slider):
 
-    layout_pie = copy.deepcopy(layout)
+#     layout_pie = copy.deepcopy(layout)
 
-    dff = filter_dataframe(df, well_statuses, well_types, year_slider)
+#     dff = filter_dataframe(df, well_statuses, well_types, year_slider)
 
-    selected = dff["API_WellNo"].values
-    index, gas, oil, water = produce_aggregate(selected, year_slider)
+#     selected = dff["API_WellNo"].values
+#     index, gas, oil, water = produce_aggregate(selected, year_slider)
 
-    aggregate = dff.groupby(["Well_Type"]).count()
+#     aggregate = dff.groupby(["Well_Type"]).count()
 
-    data = [
-        dict(
-            type="pie",
-            labels=["Gas", "Oil", "Water"],
-            values=[sum(gas), sum(oil), sum(water)],
-            name="Production Breakdown",
-            text=[
-                "Total Fluorides (mg/l)",
-                "Total Calcium-Diss (mg/l)",
-                "Total SWL (mbgs)",
-            ],
-            hoverinfo="text+value+percent",
-            textinfo="label+percent+name",
-            hole=0.5,
-            marker=dict(colors=["#fac1b7", "#a9bb95", "#92d8d8"]),
-            domain={"x": [0, 0.45], "y": [0.2, 0.8]},
-        ),
-        dict(
-            type="pie",
-            labels=[WELL_TYPES[i] for i in aggregate.index],
-            values=aggregate["API_WellNo"],
-            name="Boreholes Type Breakdown",
-            hoverinfo="label+text+value+percent",
-            textinfo="label+percent+name",
-            hole=0.5,
-            marker=dict(colors=[WELL_COLORS[i] for i in aggregate.index]),
-            domain={"x": [0.55, 1], "y": [0.2, 0.8]},
-        ),
-    ]
-    layout_pie["title"] = "Groundwater Quality Summary: {} to {}".format(
-        year_slider[0], year_slider[1]
-    )
-    layout_pie["font"] = dict(color="#777777")
-    layout_pie["legend"] = dict(
-        font=dict(color="#CCCCCC", size="10"), orientation="h", bgcolor="rgba(0,0,0,0)"
-    )
+#     data = [
+#         dict(
+#             type="pie",
+#             labels=["Gas", "Oil", "Water"],
+#             values=[sum(gas), sum(oil), sum(water)],
+#             name="Production Breakdown",
+#             text=[
+#                 "Total Fluorides (mg/l)",
+#                 "Total Calcium-Diss (mg/l)",
+#                 "Total SWL (mbgs)",
+#             ],
+#             hoverinfo="text+value+percent",
+#             textinfo="label+percent+name",
+#             hole=0.5,
+#             marker=dict(colors=["#fac1b7", "#a9bb95", "#92d8d8"]),
+#             domain={"x": [0, 0.45], "y": [0.2, 0.8]},
+#         ),
+#         dict(
+#             type="pie",
+#             labels=[WELL_TYPES[i] for i in aggregate.index],
+#             values=aggregate["API_WellNo"],
+#             name="Boreholes Type Breakdown",
+#             hoverinfo="label+text+value+percent",
+#             textinfo="label+percent+name",
+#             hole=0.5,
+#             marker=dict(colors=[WELL_COLORS[i] for i in aggregate.index]),
+#             domain={"x": [0.55, 1], "y": [0.2, 0.8]},
+#         ),
+#     ]
+#     layout_pie["title"] = "Groundwater Quality Summary: {} to {}".format(
+#         year_slider[0], year_slider[1]
+#     )
+#     layout_pie["font"] = dict(color="#777777")
+#     layout_pie["legend"] = dict(
+#         font=dict(color="#CCCCCC", size="10"), orientation="h", bgcolor="rgba(0,0,0,0)"
+#     )
 
-    figure = dict(data=data, layout=layout_pie)
-    return figure
+#     figure = dict(data=data, layout=layout_pie)
+#     return figure
 
 
 # Selectors -> count graph
@@ -700,7 +700,7 @@ def make_count_figure(well_statuses, well_types, year_slider):
             hoverinfo="skip",
         ),
         dict(
-            type="bar",
+            type="bar", # bar to line
             x=g.index,
             y=g["API_WellNo"],
             name="All Boreholes",
